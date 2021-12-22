5b212edb0c5a7fe9e28f528e845510c24152ead4
Vitor Monteiro Mendonça <monteiromendoncavitor@gmail.com>
2021-12-22 17:55:08 -0300

    Complete and Delete Todo

--- src/index.js
+++ src/index.js
@@ -24,6 +24,21 @@ function checksExistsUserAccount(request, response, next) {
   return next();
 }
 
+function checksExistsTodo(request, response, next) {
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  request.todoIndex = todoIndex;
+
+  return next();
+}
+
 app.post("/users", (request, response) => {
   const { name, username } = request.body;
 
@@ -80,40 +95,56 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
   return response.status(201).json(newTodo);
 });
 
-app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  const { user } = request;
-  const { deadline, title } = request.body;
-  const { id } = request.params;
+app.put(
+  "/todos/:id",
+  checksExistsUserAccount,
+  checksExistsTodo,
+  (request, response) => {
+    const { user, todoIndex } = request;
+    const { deadline, title } = request.body;
 
-  if (!title || !deadline) {
-    return response
-      .status(400)
-      .json({ error: "Title and deadline properties cannot be empty" });
+    if (!title || !deadline) {
+      return response
+        .status(400)
+        .json({ error: "Title and deadline properties cannot be empty" });
+    }
+
+    const updatedTudo = {
+      ...user.todos[todoIndex],
+      deadline,
+      title,
+    };
+
+    user.todos[todoIndex] = updatedTudo;
+
+    return response.json(updatedTudo);
   }
+);
 
-  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+app.patch(
+  "/todos/:id/done",
+  checksExistsUserAccount,
+  checksExistsTodo,
+  (request, response) => {
+    const { user, todoIndex } = request;
 
-  if (todoIndex === -1) {
-    return response.status(404).json({ error: "Todo not found" });
+    user.todos[todoIndex].done = true;
+
+    return response.json(user.todos[todoIndex]);
   }
+);
 
-  const updatedTudo = {
-    ...user.todos[todoIndex],
-    deadline,
-    title,
-  };
+app.delete(
+  "/todos/:id",
+  checksExistsUserAccount,
+  checksExistsTodo,
+  (request, response) => {
+    const { user, todoIndex } = request;
 
-  user.todos[todoIndex] = updatedTudo;
+    user.todos.splice(todoIndex, 1);
 
-  return response.status(204).send();
-});
-
-app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+    return response.status(204).send();
+  }
+);
 
 module.exports = app;
